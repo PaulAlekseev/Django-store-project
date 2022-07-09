f1efe494a955c27c284087192b50d1512264701b
Павел Алексеев <pavel.zommerfeld@yandex.ru>
2022-07-09 19:16:57 +0300

    added basket app

--- MySite/settings.py
+++ MySite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'store.apps.StoreConfig',
+    'basket.apps.BasketConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ basket/urls.py
@@ -0,0 +1,6 @@
+
+
+app_name = 'basket'
+urlpatterns = [
+
+]
