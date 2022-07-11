af374b2ebbd45fb304efe3a154aeee9006bf6a65
Павел Алексеев <pavel.zommerfeld@yandex.ru>
2022-07-12 00:11:46 +0300

    working on PATCH for basket app

--- basket/basket.py
+++ basket/basket.py
@@ -1,19 +1,16 @@
-from django.conf import settings
-
 from store.models import Product
 
 
 class Basket:
 
+
     def __init__(self, request):
         self.session = request.session
+        self.query = None
         basket = self.session.get('basket')
         if 'basket' not in request.session:
             basket = self.session['basket'] = {}
         self.basket = basket
-
-    def _save(self):
-        self.session.modified = True
 
     def add(self, product_id, price):
         product_id = str(product_id)
@@ -21,21 +18,23 @@ class Basket:
         if product_id in self.basket:
             self.basket[product_id]['amount'] += 1
         else:
-            self.basket[product_id] = {'amount': 1, 'price': price}
-        print(self.basket)
+            self.basket[product_id] = {'amount': 1}
         self._save()
 
     def __iter__(self):
         product_ids = self.basket.keys()
 
-        products = Product.objects.filter(id__in=product_ids)
+        if self.query is None:
+            products = self._get_queryset(product_ids)
+        else:
+            products = self.query
         basket = self.basket.copy()
 
         for product in products:
-            basket[str(product.id)]["product"] = product
+            basket[str(product.id)]['product'] = product
         
         for item in basket.values():
-            item['total'] = item['amount'] * item['price']
+            item['total'] = item['amount'] * item['product'].price
             yield item
 
     def clear(self):
@@ -43,4 +42,16 @@ class Basket:
 
         self._save()
 
-    # def update():
+    def update_item(self, data, required_amount):
+        product_id = data['product_id']
+
+        self.basket[str(product_id)]['amount'] = required_amount
+
+        self._save()
+
+    def _save(self):
+        self.session.modified = True
+
+    def _get_queryset(self, product_ids):
+        self.query = Product.objects.filter(id__in=product_ids)
+        return self.query
--- basket/views.py
+++ basket/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import generic
+from django.db.models import Sum, Q
 
-from store.models import Category
 from .basket import Basket
+from store.models import Product
 
 import json
 
@@ -15,6 +16,11 @@ class BasketView(generic.list.ListView):
     def get_queryset(self):
         basket = Basket(self.request)
         return basket
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['Total_price'] = sum(item['total'] for item in context['Products'])
+        return context
 
     def post(self, request, *args, **kwargs):
         basket = Basket(request) 
@@ -29,7 +35,25 @@ class BasketView(generic.list.ListView):
     def patch(self, request, *args, **kwargs):
         basket = Basket(request)
         data = json.loads(request.body)
-        print(data)
+        product_id = data['product_id']
+        responce_data = {}
+        current_amount = basket.basket[str(product_id)]['amount']
+        required_amount = int(data['product_amount'])
 
-        return HttpResponse('1')
+        product = Product.objects.filter(id=product_id).annotate(
+                    overall_amount=Sum('storeproduct__amount', filter=Q(id__exact=product_id))
+            )[0]
+
+        difference = required_amount - current_amount
+        if product.overall_amount >= required_amount:
+            basket.update_item(data, required_amount)
+            responce_data['agreement'] = True
+            responce_data['total_product'] = required_amount * product.price
+            responce_data['total'] = int(data['total_price']) + difference * product.price
+        else:
+            responce_data['agreement'] = False
+            responce_data['amount'] = current_amount
+
+        response = JsonResponse(responce_data)
+        return response
 
