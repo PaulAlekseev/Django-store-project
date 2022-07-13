ff8fd1fe1edcb99fc89b5f8b054d8d4df80bd464
Павел Алексеев <pavel.zommerfeld@yandex.ru>
2022-07-13 21:26:33 +0300

    started working on authentication app

--- MySite/settings.py
+++ MySite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'store.apps.StoreConfig',
     'basket.apps.BasketConfig',
+    'authentication.apps.AuthenticationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
